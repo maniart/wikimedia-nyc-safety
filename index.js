f3e71c92d9d0869cc633b64783b3f16258d30145
maniart <mani.art@gmail.com>
2019-09-05 11:39:57 -0400

    Add all things.

---
+++ index.js
@@ -0,0 +1,63 @@
+'use strict';
+
+/* imports */
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const nodemailer = require('nodemailer');
+
+if (process.env.NODE_ENV === 'development') {
+  console.log('-> development environment. using `.env` file.');
+  require('dotenv').config();
+} 
+
+const user = process.env.EMAIL_USER;
+const pass = process.env.EMAIL_PASSWORD;
+const transport = nodemailer.createTransport({
+    service: 'gmail',
+    auth: { user, pass }
+});
+
+app.use(bodyParser.urlencoded({extended: false})); 
+
+app.post('/message', function (request, response) {
+  console.log(request.body); 
+  
+  response.send(`
+    <Response>
+      <Message>
+        Thank you for your report. The organizer have been notified, and they will take appropriate actions to ensure a safe experience for everyone.
+      </Message>
+    </Response>`
+  );
+  
+  const { Body: report, From: reporterNumber } = request.body;
+  
+  const message = {
+    from: 'anonymous-participant@wikimedia.org', 
+    to: 'wikimedia.nyc.organizers@gmail.com',
+    subject: 'Participant safety report',
+    text: `
+      Participant with phone number: ${reporterNumber} 
+      Sent the following report: 
+      ${report}
+    `
+  };
+
+  transport.sendMail(message, function (error, info) {
+    if (error) {
+      console.log('Failed to send email: ', error);
+    } else {
+      console.log('Successfully sent email: ', info);
+    }
+  });
+
+});
+
+app.get('/', function (request, response) {
+  response.sendFile(__dirname + '/index.html');
+});
+
+const listener = app.listen(process.env.PORT, function () {
+  console.log('Wikimedia NYC Satefy SMS Reporter is listening on: ' + listener.address().port);
+});
